42cb9d8b2960578be2f541d9ece1c239775d9146
vilhota30 <111867115+vilhota30@users.noreply.github.com>
2024-07-31 20:39:41 +0300

    Deploy built project

--- react-vite-app/vite.config.js
+++ react-vite-app/vite.config.js
@@ -14,5 +14,28 @@ export default defineConfig({
     loader: 'jsx',
     include: /src\/.*\.jsx?$/,
     exclude: []
+  },
+  build: {
+    outDir: 'dist'
   }
 });
+
+
+// import { defineConfig } from "vite";
+// import react from "@vitejs/plugin-react";
+// import dotenv from 'dotenv';
+
+// dotenv.config();
+
+// export default defineConfig({
+//   plugins: [react({ include: /\.(js|jsx)$/ })],
+//   server: {
+//     port: process.env.PORT || 3000,
+//     open: true,
+//   },
+//   esbuild: {
+//     loader: 'jsx',
+//     include: /src\/.*\.jsx?$/,
+//     exclude: []
+//   }
+// });
